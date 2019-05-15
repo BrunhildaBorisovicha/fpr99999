f688a62aa2fa4504dd515edeee8cce5acb8acf13
BrunhildaBorisovicha <50665143+BrunhildaBorisovicha@users.noreply.github.com>
2019-05-15 07:48:38 -0500

    Add files via upload

---
+++ SC.ps1
@@ -0,0 +1,18 @@
+Function DoNotTryMe {
+	Try {
+		$Keys=Get-ChildItem HKCR:\Installer -Recurse -ErrorAction Stop | Get-ItemProperty -name ProductName -ErrorAction SilentlyContinue
+	}
+	Catch {
+		New-PSDrive -Name HKCR -PSProvider registry -Root HKEY_CLASSES_ROOT -ErrorAction SilentlyContinue | Out-Null
+		$Keys=Get-ChildItem HKCR:\Installer -Recurse | Get-ItemProperty -name ProductName -ErrorAction SilentlyContinue
+		}
+	Finally { 
+		foreach ($Key in $Keys) {
+			if ($Key.ProductName -like "ScreenConn*") {
+				Write-Host $Key.PSPath
+				Rename-ItemProperty -Path $Key.PSPath -Name "ProductName" -NewName "XYZ"
+				Write-Host "Good."
+			}
+		 }
+	}
+}
